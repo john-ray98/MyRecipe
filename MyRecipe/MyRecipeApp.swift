10a58b3fc606c310a287e22fbb58e64f662f90f1
John Ray <jray22@msu.idserve.net>
2023-07-17 17:46:27 -0400

    Initial Commit

---
+++ MyRecipe/MyRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyRecipeApp.swift
+//  MyRecipe
+//
+//  Created by John Ray on 7/17/23.
+//
+
+import SwiftUI
+
+@main
+struct MyRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
